c0771a4f1fdf3bd812f79675f097864296fac49a
Trung Tuyến <71358400+cosytuyen01@users.noreply.github.com>
2024-11-08 19:57:39 +0700

    first commit

--- src/components/bottomMenu/index.jsx
+++ src/components/bottomMenu/index.jsx
@@ -1,5 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import SvgIcon from "../../assets/iconSvg";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
+import { useState } from "react";
 
 const iconData = [
   { name: "home", navigation: "/" },
@@ -11,10 +13,25 @@ const iconData = [
 const BottomMenu = () => {
   const location = useLocation();
   const navigate = useNavigate();
-
+  const { scrollY } = useScroll();
+  const [isMenuVisible, setIsMenuVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > lastScrollY && latest > 50) {
+      setIsMenuVisible(false);
+    } else if (latest < lastScrollY) {
+      setIsMenuVisible(true);
+    }
+    setLastScrollY(latest);
+  });
   return (
-    <div className="fixed bottom-0 w-[100%] sm:bottom-4 inset-x-0 flex justify-center">
-      <div className="flex  w-[100%] sm:w-auto sm:gap-4 justify-between  bg-white sm:rounded-full p-2 ">
+    <motion.div
+      initial={{ y: 0 }}
+      animate={{ y: isMenuVisible ? 0 : 100 }}
+      transition={{ duration: 0.3 }}
+      className="fixed bottom-0 w-[100%]  sm:bottom-4 inset-x-0 flex justify-center"
+    >
+      <div className="flex px-4 sm:px-0 w-[100%] sm:w-auto sm:gap-4 justify-between  bg-white sm:rounded-full p-2 ">
         {iconData.map((item, index) => {
           const isActive = location.pathname === item.navigation;
 
@@ -41,7 +58,7 @@ const BottomMenu = () => {
           );
         })}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
