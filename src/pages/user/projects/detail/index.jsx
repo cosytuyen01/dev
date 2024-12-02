6433f58ca1d0231f82665846a63bb926f73ed4ae
Trung Tuyến <71358400+cosytuyen01@users.noreply.github.com>
2024-12-02 21:48:07 +0700

    deploy

--- src/pages/user/projects/detail/index.jsx
+++ src/pages/user/projects/detail/index.jsx
@@ -1,22 +1,37 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { products } from "../data";
 import SvgIcon from "../../../../assets/iconSvg";
-
+import Modal from "react-modal";
+import "./style.css";
+Modal.setAppElement("#root");
 const DetailProject = () => {
   const { pathname } = useLocation();
   const { id } = useParams();
   const navigate = useNavigate();
   const [iconColor, setIconColor] = useState("black");
-
+  const [selectedImage, setSelectedImage] = useState(0); // Ảnh đang được chọn
+  const [isModalOpen, setIsModalOpen] = useState(false); // Trạng thái hiển thị modal
   const productData = products.find((product) => product.id === parseInt(id));
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+  useEffect(() => {
+    // Ngừng cuộn khi modal mở
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
 
+    return () => {
+      // Trả lại trạng thái cuộn khi component bị unmount hoặc khi modal đóng
+      document.body.style.overflow = "auto";
+    };
+  }, [isModalOpen]);
   useEffect(() => {
     const updateIconColor = () => {
       if (document.documentElement.classList.contains("dark")) {
@@ -43,7 +58,25 @@ const DetailProject = () => {
   const handleBackClick = () => {
     navigate(-1);
   };
+  const closeModal = () => {
+    setIsModalOpen(false); // Đóng modal
+  };
+  const openModal = (index) => {
+    setSelectedImage(index); // Cập nhật ảnh được chọn
+    setIsModalOpen(true); // Mở modal
+  };
 
+  const imageRefs = useRef([]);
+
+  const handleImageClick = (index) => {
+    setSelectedImage(index);
+    // Tự động cuộn đến ảnh đã chọn
+    imageRefs.current[index].scrollIntoView({
+      behavior: "smooth",
+      block: "center", // Cuộn đến vị trí giữa
+      inline: "center", // Cuộn ảnh vào giữa theo chiều ngang
+    });
+  };
   return (
     <div className="px-4 sm:pt-[40px] lg:px-0 pt-[20px] flex flex-col items-center w-full lg:w-[752px] h-[100%]">
       <div
@@ -115,6 +148,7 @@ const DetailProject = () => {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true, amount: 0.2 }}
               transition={{ duration: 0.5 }}
+              onClick={() => openModal(index)}
             />
           ))}
         </div>
@@ -131,9 +165,52 @@ const DetailProject = () => {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true, amount: 0.2 }}
               transition={{ duration: 0.5 }}
+              onClick={() => openModal(index)}
             />
           ))}
         </div>
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 "
+          overlayClassName="fixed inset-0 dark:bg-black/20 bg-white/20 backdrop-blur"
+        >
+          <div className="relative  rounded-lg max-w-3xl ">
+            <button
+              onClick={closeModal}
+              className="absolute top-2 right-0 p-2 rounded-full dark:bg-white bg-black mr-4"
+            >
+              <SvgIcon
+                name={"close"}
+                height={24}
+                width={24}
+                color={iconColor}
+              />
+            </button>
+            {/* Ảnh phóng to */}
+            <img
+              src={productData?.images[selectedImage]}
+              alt={`image-${selectedImage}`}
+              className="w-[100vw] h-[80vh] object-contain rounded-lg "
+            />
+            <div className="mt-4 flex gap-2  scroll-container hide-scrollbar">
+              {productData?.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`image-${index}`}
+                  className={`w-16 h-16 object-cover cursor-pointer transition-all duration-100 rounded-lg ${
+                    selectedImage === index
+                      ? "border-4 border-blue-500 shadow-lg"
+                      : "opacity-50"
+                  }`}
+                  ref={(el) => (imageRefs.current[index] = el)}
+                  onClick={() => handleImageClick(index)} // Thay đổi ảnh khi nhấn vào ảnh nhỏ
+                />
+              ))}
+            </div>
+          </div>
+        </Modal>
       </div>
     </div>
   );
